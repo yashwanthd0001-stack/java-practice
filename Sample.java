c43a3be5757a723368b01c8457298f7acd78e369
yashwanthd0001-stack <yashwanthd0001@gmail.com>
2025-12-15 08:32:05 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,12 @@
+class Add2no
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(20+20);
+		System.out.println("The value is"+20);
+		System.out.println("The value is"+20+20);
+		System.out.println("The value is"+(20+20));
+		System.out.println(20+"is the value");
+		System.out.println(20+20+","+"is the value");
+	}
+}
