28412efc2649ba1fcb272916f49ee05bebd372e0
yashwanthd0001-stack <yashwanthd0001@gmail.com>
2025-12-15 08:20:01 +0530

    Add files via upload

---
+++ Pliterals.java
@@ -0,0 +1,14 @@
+class Pliterals 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(20);
+		System.out.println(20.20);
+		System.out.println("Hello");
+		System.out.println('a');
+		System.out.println(true);
+	    		
+		
+
+	}
+}
