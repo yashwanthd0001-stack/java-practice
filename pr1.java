31771bcfbfad07ce8ddf25ef522253af6270e9a2
yashwanthd0001-stack <yashwanthd0001@gmail.com>
2025-12-29 12:36:32 +0530

    Add files via upload

---
+++ pr1.java
@@ -0,0 +1,13 @@
+class pr1
+{
+    public static void main(String[]args)
+    {
+        for(int i=8;i<=10;i++)
+        {
+            if(i>=5)
+            {
+                System.out.print("ok"+(i-5)+" ");
+            }
+        }
+    }
+}
